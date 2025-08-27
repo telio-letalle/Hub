5179ec23acd91889333e7705e31db1ee4108a6a6
telio-letalle <telioletallesae@gmail.com>
2025-08-27 14:50:37 +0200

    Nouvelle amélioration : Style Visuel

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,28 @@ import controller.HubController;
 import model.HubModel;
 import view.HubView;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
+        try {
+            // Active Nimbus si dispo
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // Si Nimbus n’est pas dispo → Swing classique
+            e.printStackTrace();
+            System.out.println("Error loading LookAndFeel");
+        }
+
+        SwingUtilities.invokeLater(() -> {
             HubModel model = new HubModel();
             HubView view = new HubView();
             new HubController(view, model);
-
             view.setVisible(true);
         });
     }
